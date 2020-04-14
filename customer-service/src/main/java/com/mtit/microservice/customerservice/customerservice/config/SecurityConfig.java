4c1cdc27ce1cc9340e4ef8bf9d8fd75742da25f3
dilshan97 <dilshanramesh81@gmail.com>
2020-04-14 16:08:41 +0530

    security config updated

--- customer-service/src/main/java/com/mtit/microservice/customerservice/customerservice/config/SecurityConfig.java
+++ customer-service/src/main/java/com/mtit/microservice/customerservice/customerservice/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate","/register")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
