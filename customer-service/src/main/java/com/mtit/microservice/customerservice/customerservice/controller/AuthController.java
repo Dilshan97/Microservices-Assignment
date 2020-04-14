e65f2c569f31e849fc5aaf77b044d5a67a175d7d
dilshan97 <dilshanramesh81@gmail.com>
2020-04-14 12:42:35 +0530

    missing dependancy added

---
+++ customer-service/src/main/java/com/mtit/microservice/customerservice/customerservice/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.mtit.microservice.customerservice.customerservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/")
+    public String index(){
+        return "Hello Spring Boot !";
+    }
+}
