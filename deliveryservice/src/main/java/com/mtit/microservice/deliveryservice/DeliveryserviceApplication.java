73436d132b5941be74882a9339fbc9743a826ec2
dilshan97 <dilshanramesh81@gmail.com>
2020-04-17 23:47:18 +0530

    delivery service added

---
+++ deliveryservice/src/main/java/com/mtit/microservice/deliveryservice/DeliveryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mtit.microservice.deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryserviceApplication.class, args);
+	}
+
+}
---
+++ deliveryservice/src/test/java/com/mtit/microservice/deliveryservice/DeliveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtit.microservice.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
