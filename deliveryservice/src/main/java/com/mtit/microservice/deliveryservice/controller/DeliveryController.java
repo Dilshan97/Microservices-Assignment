e63c5682739fcebb9977a05486d8534758b0ae24
dilshan97 <dilshanramesh81@gmail.com>
2020-04-18 15:56:58 +0530

    authorization headers bug fixed

--- deliveryservice/src/main/java/com/mtit/microservice/deliveryservice/controller/DeliveryController.java
+++ deliveryservice/src/main/java/com/mtit/microservice/deliveryservice/controller/DeliveryController.java
@@ -6,6 +6,8 @@ import com.mtit.microservice.deliveryservice.service.DeliveryServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpHeaders;
+
 @RestController
 public class DeliveryController {
 
@@ -20,11 +22,11 @@ public class DeliveryController {
 
     //Get Customer Details API end-point
     @PostMapping(value = "/get-delivery-details", consumes = "application/json", produces = "application/json")
-    public @ResponseBody DeliveryResponse getDeliveryDetails(@RequestBody DeliveryRequest deliveryRequest){
+    public @ResponseBody DeliveryResponse getDeliveryDetails(@RequestBody DeliveryRequest deliveryRequest, @RequestHeader(name = "Content-Type") String Content, @RequestHeader(name = "Authorization") String Authorization){
 
         System.out.println(deliveryRequest);
 
-        return deliveryService.getDeliveryDetails(deliveryRequest);
+        return deliveryService.getDeliveryDetails(deliveryRequest, Content, Authorization);
 
     }
 }
